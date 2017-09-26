813cf866da81934d68ce6fa27b587de7deacfad8
Joanna Trojak <joannatrojak19@gmail.com>
2017-09-26 11:32:06 +0200

    Item - getters and setters

--- Item.php
+++ Item.php
@@ -14,4 +14,31 @@ class Item
         $this->price = 0; 
         $this->amount = 0; 
     }
+    public function getId() {
+        return $this->id; 
+    }
+    public function setName($name) {
+        $this->name = $name; 
+    }
+    public function getName() {
+        return $this->name; 
+    }
+    public function setDescription($description) {
+        $this->description = $description; 
+    }
+    public function getDescription() {
+        return $this->description; 
+    }
+    public function setPrice($price) {
+        $this->price = $price; 
+    }
+    public function getPrice() {
+        return $this->price; 
+    }
+    public function setAmount($amount) {
+        $this->amount = $amount; 
+    }
+    public function getAmount() {
+        return $this->amount; 
+    }
 }
