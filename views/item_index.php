b440eaa32800f65982cd6ac211dbede8a054177e
Joanna Trojak <joannatrojak19@gmail.com>
2017-09-28 13:37:54 +0200

    products index

---
+++ views/item_index.php
@@ -0,0 +1,20 @@
+<?php
+include "config.php";
+include 'class/Item.php';
+?>
+<!DOCTYPE>
+<html>
+    <head>
+        <title>
+            Products Index
+        </title>
+    </head>
+    <body>
+        <h1>Products</h1>
+        <table>
+            
+        </table>
+    </body>
+</html>
+
+
