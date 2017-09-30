ddef4c4018a9d79032e68397a023d183358d9454
Joanna Trojak <joannatrojak19@gmail.com>
2017-09-30 13:41:00 +0200

    test load all items

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -31,7 +31,20 @@ class ItemTest extends TestCase
         $item->setDescription('śliskie'); 
         $this->assertEquals('śliskie', $item->getDescription()); 
         $item->saveToDB(new mysqli('localhost', 'root', 'coderslab', 'shop_tests')); 
-        $this->assertEquals(2, $this->getConnection()->getRowCount('products')); 
-        
+        $this->assertEquals(2, $this->getConnection()->getRowCount('products'));      
+    }
+    public function testShouldLoadAllItems() {
+        $item = new Item();
+        $item->setName('cos'); 
+        $item->setAmount(10); 
+        $item->setPrice(1); 
+        $item->setDescription('cos tam'); 
+        $item->saveToDB(new mysqli('localhost', 'root', 'coderslab', 'shop_tests')); 
+        $item->loadAllItems(new mysqli('localhost', 'root', 'coderslab', 'shop_tests')); 
+        $this->assertNotEmpty($item); 
+        $this->assertInstanceOf(Item::class, $item); 
+        $this->assertNotEmpty($item); 
+        $this->assertArrayHasKey(0, $item->loadAllItems(new mysqli('localhost', 'root', 'coderslab', 'shop_tests'))); 
+       $this->assertInternalType("array", $item->loadAllItems(new mysqli('localhost', 'root', 'coderslab', 'shop_tests'))); 
     }
 }
